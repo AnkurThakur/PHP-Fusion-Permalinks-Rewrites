2483a7bab5a25149e4584b43a50309bf6b339ca7
Ankur Thakur <thakurboyz2007@gmail.com>
2013-05-20 14:39:41 +0530

    Module Include Files for Permalinks and Rewrites
    Module Include Files for Permalinks and Rewrites
    These files are included by the Engine for processing different links in
    website. Each link has different structure for SEO link. All the
    definition is made inside this.

---
+++ includes/rewrites/articles_rewrite_include.php
@@ -0,0 +1,34 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) 2002 - 2011 Nick Jones
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: articles_rewrite_include.php
+| Author: Ankur Thakur
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
+
+$regex = array(
+	"%article_id%" => "([0-9]+)",
+	"%article_title%" => "([a-zA-Z0-9-_]+)"
+);
+$pattern = array(
+	"%article_id%/article/%article_title%" => "articles.php?article_id=%article_id%"
+);
+$dir_path = ROOT;
+$dbname = DB_ARTICLES;
+$dbid = array("%article_id%" => "article_id");
+$dbinfo = array(
+	"%article_title%" => "article_subject"
+);
+
+?>
---
+++ includes/rewrites/news_rewrite_include.php
@@ -0,0 +1,41 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) 2002 - 2011 Nick Jones
+| http://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: news_rewrite_include.php
+| Author: Ankur Thakur
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+if (!defined("IN_FUSION")) { die("Access Denied"); }
+
+$regex = array(
+	"%news_id%" => "([0-9]+)",
+	"%news_title%" => "([a-zA-Z0-9-]+)"
+);
+$pattern = array(
+	"news" => "news.php",
+	"news/%news_id%/%news_title%" => "news.php?readmore=%news_id%",
+	"news/%news_id%/%news_title%#comments" => "news.php?readmore=%news_id%#comments"
+);
+$alias_pattern = array(
+	"news/%alias%" => "%alias_target%",
+	"news/%alias%#comments" => "%alias_target%#comments"
+);
+$dir_path = ROOT;
+$dbname = DB_NEWS;
+$dbid = array("%news_id%" => "news_id");
+$dbinfo = array(
+	"%news_title%" => "news_subject",
+	"%news_start%" => "news_start"
+);
+
+?>
